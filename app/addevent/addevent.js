737690dac7618e2a1a9eb28d8c75a2f3fe6c99ec
Quinn Vissak <Quinn Vissak>
2016-10-08 22:42:00 -0400

    Create autosuggest google api application

--- app/addevent/addevent.js
+++ app/addevent/addevent.js
@@ -1,14 +1,23 @@
 'use strict';
 
-angular.module('myApp.addevent', ['ngRoute'])
+angular.module('myApp.addevent', ['ngRoute', 'ngMap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/addevent', {
     templateUrl: 'addevent/addevent.html',
-    controller: 'addEventCtrl as ctrl'
+    controller: 'addEventCtrl as vm'
   });
 }])
 
-.controller('addEventCtrl', [function() {
-
+.controller('addEventCtrl', ['NgMap', function(NgMap) {
+	var vm = this;
+  vm.types = "['establishment']";
+  vm.placeChanged = function() {
+    vm.place = this.getPlace();
+    console.log('location', vm.place.geometry.location);
+    vm.map.setCenter(vm.place.geometry.location);
+  }
+  NgMap.getMap().then(function(map) {
+    vm.map = map;
+  });
 }]);
