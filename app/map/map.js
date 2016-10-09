77cbe4d940cf4f40d70d28b719e9968b5f13eff6
dwminer <daltonminer@gmail.com>
2016-10-08 23:33:15 -0400

    Add search/category filtering to map

--- app/map/map.js
+++ app/map/map.js
@@ -15,13 +15,58 @@ angular.module('myApp.map', ['ngRoute','ngMap', 'ngMaterial'])
 	self.events = [
 		{
 			name: 'Hack Umass',
-			location: [42.391, -72.526]
+			location: [42.391, -72.526],
+			location_desc: 'Integrative Learning Center',
+			time: new Date(),
+			category: 'Other'
 		},
 		{
 			name: 'Chill @ Sam\'s',
-			location: [42.388570, -72.529235]
+			location: [42.387095, -72.530784],
+			location_desc: 'Linden Hall',
+			time: new Date(),
+			category: 'Other'
+		},
+		{
+			name: 'KanJam on the green!',
+			location: [42.389392, -72.525616],
+			location_desc: 'Green by the lake',
+			time: new Date(),
+			category: 'Sports'
+		},
+		{
+			name: 'Study session for Algorithms. :(',
+			location: [42.393957, -72.529548],
+			location_desc: 'School of Engineering building',
+			time: new Date(),
+			category: 'Study'
+		},
+		{
+			name: 'Prepare for physics',
+			location: [42.394599, -72.530642],
+			location_desc: 'ME Building',
+			time: new Date(),
+			category: 'Study'
 		}
 	];
+
+	self.categories = ['Sports', 'Music', 'Video games', 'Board games', 'Outing', 'Social', 'Study', 'Volunteer', 'Other'];
+	self.sel_categories = {};
+	for (var cat in self.categories){
+		self.sel_categories[self.categories[cat]] = true;
+	}
+
+	self.distance = function(p1, p2) {
+		return math.sqrt(p1*p1 + p2*p2);
+	}
+
+	self.comparator = function(actual, expected) {
+		console.log(self.sel_categories[actual.category]);
+	}
+
+	self.change = function() {
+		console.log(self.sel_categories);
+	}
 	
 	$http.get('/secrets.json').then(function successCallback(response){
 		self.key = response.data.key;
@@ -38,4 +83,43 @@ angular.module('myApp.map', ['ngRoute','ngMap', 'ngMaterial'])
 		});
   	});
 
-}]);
+}])
+.filter('timefilter', function() {
+	return function(dateobj) {
+		var now = new Date();
+		if (dateobj.getTime() <= now.getTime())
+		{
+			return "now!";
+		}
+		else if (dateobj.getTime() - now.getTime() <= 60 * 60 * 1000)
+		{
+			return "in " + (dateobj.getTime() - now.getTime())/(60*1000) + " minutes!";
+		}
+		else if (dateobj.getDay() == now.getDay() && dateobj.getMonth() == now.getMonth())
+		{
+			return "in " + (dateobj.getTime() - now.getTime())/(60*60*1000) + " hours.";
+		}
+		else
+		{
+			return dateobj.getHours() + ":" + dateobj.getMinutes() + " on " + dateobj.getMonth() + "/" + dateobj.getDay() + ".";
+		}
+	}
+})
+.filter('eventFilter', function() {
+	return function(input, expected, sel_categories) {
+		var res = [];
+
+		if (!expected)
+			expected = {$:""}
+		
+		angular.forEach(input, function(event) {
+			if ((event.name.toLowerCase().match(expected.$.toLowerCase()) || event.location_desc.toLowerCase().match(expected.$.toLowerCase())) &&
+				sel_categories[event.category])
+			{
+				res.push(event);
+			}
+		})
+
+		return res;
+	}
+});
